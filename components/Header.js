9e173d1455d5f0821b047452238557c997c970aa
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-07 07:56:23 +0100

    styling edits

--- components/Header.js
+++ components/Header.js
@@ -2,21 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="flex justify-between align-baseline mb-20">
-            <Link href="/">
-                <a className="flex space-x-2 text-base">
-                    {/* svg with left facing arrow */}
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
-                    </svg>
-                    Back to all patterns
-                </a>
-            </Link>
+        <header className="flex justify-center align-baseline mb-20">
             <Link href=" /">
                 <a>
                     <h3 className="text-base font-mediumBlue font-bold tracking-wide">
